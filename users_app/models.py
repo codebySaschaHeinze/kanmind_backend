b3385e9912e8a97e58cdfde529c1315795e96b31
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-01-29 01:05:29 +0100

    add User model

--- users_app/models.py
+++ users_app/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    fullname = models.CharField(max_length=150)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["fullname"]
+
+    def __str__(self):
+        return f"{self.fullname} <{self.email}>"
