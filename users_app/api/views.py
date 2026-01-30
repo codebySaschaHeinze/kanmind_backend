d95faf2f4317f0d43e38853b0074634b6db07dc3
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-01-30 15:35:45 +0100

    add EmailCheckView in users_app

--- users_app/api/views.py
+++ users_app/api/views.py
@@ -1,10 +1,13 @@
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
 
 from .serializers import RegistrationSerializer, LoginSerializer
+
+User = get_user_model()
 
 
 class RegistrationView(APIView):
@@ -29,7 +32,7 @@ class RegistrationView(APIView):
     
 
 class LoginView(APIView):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
@@ -47,3 +50,23 @@ class LoginView(APIView):
             },
             status=status.HTTP_200_OK,
         )
+    
+
+class EmailCheckView():
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        email = request.query_params.get("email")
+        if not email:
+            return Response({"detail": "Email Abfrageparameter erforderlich."},
+                status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(email=email)
+            return Response(
+                {"id": user.id, "email": user.email, "fullname": user.fullname},
+                status=status.HTTP_200_OK,
+            )   
+        except User.DoesNotExist:
+            return Response({"detail": "Benutzer wurde nicht gefunden."}, 
+                status=status.HTTP_404_NOT_FOUND)
