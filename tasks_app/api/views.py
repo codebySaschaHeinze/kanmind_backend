3adc6a73830f9a49466d35332e3c9c9c6d4980e3
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-01-30 17:43:17 +0100

    add CommentViewSet in tasks_app

--- tasks_app/api/views.py
+++ tasks_app/api/views.py
@@ -4,8 +4,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from boards_app.models import Board
-from tasks_app.models import Task
-from .serializers import TaskSerializer
+from tasks_app.models import Task, Comment
+from .serializers import TaskSerializer, CommentSerializer
 from .permissions import IsTaskBoardMember
 
 
@@ -24,11 +24,14 @@ class TaskViewSet(viewsets.ModelViewSet):
     
     def create(self, request, *args, **kwargs):
         board_id = request.data.get("board")
+        
         if not board_id:
             return Response({"detail": "Ein Board ist erforderlich."}, 
                 status=status.HTTP_400_BAD_REQUEST)
+        
         try:
             board = Board.objects.get(id=board_id)
+        
         except Board.DoesNotExist:
             return Response({"detail": "Board wurde nicht gefunden."},
                 status=status.HTTP_404_NOT_FOUND)
@@ -38,6 +41,7 @@ class TaskViewSet(viewsets.ModelViewSet):
                 status=status.HTTP_403_FORBIDDEN)
         
         return super().create(request, *args, **kwargs)
+    
     
 @action(detail=False, methods=["get"], url_path="assigned-to-me")
 def assigned_to_me(self, request):
@@ -51,3 +55,41 @@ def reviewing(self, request):
     queryset = self.get_queryset().filter(reviewer=request.user)
     serializer = self.get_serializer(queryset, many=True)
     return Response(serializer.data)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        task_id = self.kwargs["task_id"]
+        user = self.request.user
+        return Comment.objects.filter(task_id=task_id, task__board__members=user).distinct()
+    
+    def create(self, request, *args, **kwargs):
+        task_id = self.kwargs["task_id"]
+        user = request.user
+
+        try:
+            task = Task.objects.get(id=task_id)
+
+        except Task.DoesNotExist:
+            return Response({"detail": "Task wurde nicht gefunden."},
+                status=status.HTTP_404_NOT_FOUND)
+        
+        if not task.board.members.filter(id=user.id).exists():
+            return Response({"detail": "Du bist kein Board-Member."},
+                status=status.HTTP_403_FORBIDDEN)
+        
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        comment = Comment.object.create(
+            task=task,
+            author=user,
+            text=serializer.validated_data["text"],
+        )
+
+        comment_output = CommentSerializer(comment).data
+        return Response(comment_output, 
+                status=status.HTTP_201_CREATED)
+    
