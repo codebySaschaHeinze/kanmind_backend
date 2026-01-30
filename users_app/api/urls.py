076593178f39d11f71840139c6dad141e3ab7b61
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-01-30 16:05:25 +0100

    set EmailCheckView routing

--- users_app/api/urls.py
+++ users_app/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import RegistrationView, LoginView
-
+from .views import LoginView, RegistrationView, EmailCheckView
 
 urlpatterns = [
     path("registration/", RegistrationView.as_view()),
     path("login/", LoginView.as_view()),
+    path("email-check/", EmailCheckView.as_view()),
 ]
